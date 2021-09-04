c2a2ec73e3307d05b064b1d0f4df72ef1ea1e475
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-04 16:31:18 +0500

    implement generic helper function to upload images

--- helper/functions.py
+++ helper/functions.py
@@ -1,4 +1,36 @@
+from django.conf import settings
 from random import randint
+import logging
+import boto3
+from botocore.exceptions import ClientError
 
 def generateOtp():
     return randint(1000,9999)
+
+
+
+
+def create_presigned_url(object_name, bucket_name=settings.BUCKET_NAME,
+                        fields=None, conditions=None, expiration=3600):
+    """Generate a presigned URL S3 POST request to upload a file
+    
+    :return: Dictionary with the following keys:
+        url: URL to post to
+        fields: Dictionary of form fields and values to submit with the POST
+    :return: None if error.
+    """
+
+    # Generate a presigned S3 POST URL
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_post(bucket_name,
+                                                    object_name,
+                                                    Fields=fields,
+                                                    Conditions=conditions,
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL and required fields
+    return response
