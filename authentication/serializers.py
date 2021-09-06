ac67e4049472f7c194c4db016fe3b0dd8926995d
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-06 21:50:30 +0500

    removed otp verification from login serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -55,39 +55,6 @@ class LoginAndObtainTokenSerializer(TokenObtainPairSerializer):
                 }]
             }
 
-        if (self.user.email == self.initial_data['email'] and  
-            self.user.otp == self.initial_data.get('otp',None)
-        ):
-            User.objects.filter(
-                Q(email=self.initial_data['email']) | Q(otp=self.initial_data['otp'])
-            ).update(otp=None, isVerified=True)
-
-            # intializing candidate related models
-
-            details_instance = Details.objects.create(user=self.user)
-            PhoneNumbers.objects.create(details=details_instance)
-            Address.objects.create(details=details_instance)
-            
-            for exam_year in ExamYear.objects.all():
-                academic_instance = Academics.objects.create()
-                UserAcademicRecord.objects.create(examYear=exam_year, academics=academic_instance,user=self.user)
-                
-            Experience.objects.create(user=self.user)
-
-            refresh = self.get_token(self.user)
-            
-            return {
-                "success": True,
-                "message": "Logged In",
-                "data": [{
-                    "email": self.user.email,
-                    "isAdmin": self.user.isAdmin,
-                    "appId": self.user.appId,
-                    "access": str(refresh.access_token),
-                    "isVerified": self.user.isVerified,
-                    "refresh": str(refresh)
-                }]
-            }
         return {
             "success": False,
             "message": "please verify your email first",
@@ -114,7 +81,9 @@ class VerifyEmailAndObtainTokenSerializer(TokenObtainPairSerializer):
 
             # intializing candidate related models
 
-            Details.objects.create(user=self.user)
+            details_instance=Details.objects.create(user=self.user)
+            PhoneNumbers.objects.create(details=details_instance)
+            Address.objects.create(details=details_instance)
             
             for exam_year in ExamYear.objects.all():
                 academic_instance = Academics.objects.create()
