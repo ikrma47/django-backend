4d849f5cb593f4aa2c3d4188fdf4d6def14156e6
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-06 19:03:57 +0500

    implemented verify email endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
-from .views import SignupView, LoginView
+from .views import SignupView, LoginView, VerifyEmailView
 from django.urls import path
 
 
 urlpatterns = [
     path('api/signup/', SignupView.as_view({'post': 'create'})),
-    path('api/login/', LoginView.as_view())
+    path('api/login/', LoginView.as_view()),
+    path('api/verify-email/otp/', VerifyEmailView)
 ]
