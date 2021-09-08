7449e86a1210a1ce991ec95efd1cdfed11607aef
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-08 21:07:19 +0500

    secured proctected views with jwt auth

--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -2,11 +2,16 @@ from rest_framework import viewsets, mixins
 from .serializers import DashboardSerializer
 from user_details.models import Details
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
 
 
 class DashboardView(viewsets.GenericViewSet, mixins.RetrieveModelMixin):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     serializer_class = DashboardSerializer
     queryset = Details.objects.all()
