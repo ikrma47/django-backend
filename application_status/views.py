57e94635985d4dfffbc8cf7108bff9fb2277a07c
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-08 12:14:48 +0500

    initialized application status viewset

--- application_status/views.py
+++ application_status/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from .serializers import ApplicationStatusSerializer
+from .models import ApplicationStatus
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class ApplicationStatusView(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
+
+    serializer_class = ApplicationStatusSerializer
+    queryset = ApplicationStatus.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(data={
+            'success': True,
+            'message': 'successfully fetched',
+            'data': [serializer.data],
+        })
