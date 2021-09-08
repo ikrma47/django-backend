35a7f24e5460db1d7a3a0e361b6338a0252c8942
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-08 13:33:34 +0500

    protected secured views with jwt authentications

--- application_status/views.py
+++ application_status/views.py
@@ -2,11 +2,16 @@ from rest_framework import viewsets, mixins
 from .serializers import ApplicationStatusSerializer
 from .models import ApplicationStatus
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
 
 
 class ApplicationStatusView(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     serializer_class = ApplicationStatusSerializer
     queryset = ApplicationStatus.objects.all()
