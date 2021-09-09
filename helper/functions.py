85782ad55b7cc126fe1e6ed0f0b49b331f55616e
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-09 11:23:03 +0500

    rectified the signed request

--- helper/functions.py
+++ helper/functions.py
@@ -10,31 +10,50 @@ def generateOtp():
     return randint(1000, 9999)
 
 
-def create_presigned_url(object_name, bucket_name=settings.BUCKET_NAME, expiration=3600):
+def create_presigned_url(
+    object_name,
+    object_type,
+    bucket_name=settings.BUCKET_NAME,
+    expiration=3600
+):
+
+    # added a random number to file name to have unique file name
+    object_name = object_name[:object_name.rfind(
+        '.')] + str(generateOtp()) + str(generateOtp()) + object_name[object_name.rfind('.'):]
+
     """Generate a presigned URL S3 POST request to upload a file
 
-    :return: Dictionary with the following keys:
-        url: URL to post to
-        fields: Dictionary of form fields and values to submit with the POST
     :return: None if error.
     """
 
     # Generate a presigned S3 POST URL
-    s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-                             region_name=settings.AWS_REGION_NAME
-                             )
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_REGION_NAME,
+    )
     try:
-        response = s3_client.generate_presigned_url('put_object',
-                                                    Params={'Bucket': bucket_name,
-                                                            'Key': object_name},
-                                                    ExpiresIn=expiration)
+        response = s3_client.generate_presigned_url(
+            'put_object',
+            {
+                'Bucket': bucket_name,
+                'Key': object_name,
+                'ACL': 'public-read',
+                'ContentType': object_type
+            },
+            ExpiresIn=expiration,
+        )
+
     except ClientError as e:
         logging.error(e)
         return None
 
     # The response contains the presigned URL and required fields
-    return response
+    return {
+        "signedRequest": response,
+        'url': f'https://{bucket_name}.s3.{settings.AWS_REGION_NAME}.amazonaws.com/{object_name}'
+    }
 
 
 def custom_exception_handler(exc, context):
