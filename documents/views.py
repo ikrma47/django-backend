1afd8aaaccc0af3faee2c8c2a5d0e2c9082ed4a9
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-09 21:21:25 +0500

    override method to customized the response

--- documents/views.py
+++ documents/views.py
@@ -10,3 +10,26 @@ class DocumentsView(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.U
 
     serializer_class = DocumentsSerializer
     queryset = Documents.objects.all()
+    lookup_field = 'appId'
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(data={
+            'success': True,
+            'message': 'documents fetched',
+            'data': [serializer.data]
+        })
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(data={
+            'success': True,
+            'message': 'documents updated successfully',
+            'data': [serializer.data]
+        })
