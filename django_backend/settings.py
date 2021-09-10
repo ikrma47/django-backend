b49b070529442ddb5969b4dec4a38c8635ebde88
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-10 15:08:38 +0500

    override the default filter backends

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -87,6 +87,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
     'EXCEPTION_HANDLER': 'helper.functions.custom_exception_handler'
 }
 
