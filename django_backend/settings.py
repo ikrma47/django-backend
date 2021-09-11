ff2b04ffe4d2001d004427eba5f8e7f84838f663
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-11 09:36:54 +0500

    set frontend url as env

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('FRONTEND_URL')]
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
