cd80a11e919199f330209c0e2a74996566518661
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-11 10:08:07 +0500

    set debug to boolean

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG')
+DEBUG = env('DJANGO_DEBUG') != 'False'
 
 ALLOWED_HOSTS = [env('FRONTEND_URL')]
 CORS_ORIGIN_ALLOW_ALL = True
